8f8d3714f22c6902aef8dd65b40b3d13cf678873
Evgeniy Budaev <budaev.e@gmail.com>
2023-10-24 19:44:08 +0300

    feat: 36. Advanced testing of authorized handlers

--- api/booking_handler_test.go
+++ api/booking_handler_test.go
@@ -1,20 +1,114 @@
 package api
 
 import (
+	"encoding/json"
 	"fmt"
+	"github.com/EvgeniyBudaev/golang-hotel-reservation/api/middleware"
 	"github.com/EvgeniyBudaev/golang-hotel-reservation/internal/db/fixtures"
+	"github.com/EvgeniyBudaev/golang-hotel-reservation/internal/models"
+	"github.com/gofiber/fiber/v2"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 	"time"
 )
 
+func TestUserGetBooking(t *testing.T) {
+	db := setup(t)
+	defer db.teardown(t)
+	var (
+		nonAuthUser    = fixtures.AddUser(db.Store, "Jimmy", "watercooler", false)
+		user           = fixtures.AddUser(db.Store, "james", "foo", false)
+		hotel          = fixtures.AddHotel(db.Store, "bar hotel", "a", 4, nil)
+		room           = fixtures.AddRoom(db.Store, "small", true, 4.4, hotel.ID)
+		from           = time.Now()
+		till           = from.AddDate(0, 0, 5)
+		booking        = fixtures.AddBooking(db.Store, user.ID, room.ID, from, till)
+		app            = fiber.New()
+		route          = app.Group("/", middleware.JWTAuthentication(db.User))
+		bookingHandler = NewBookingHandler(db.Store)
+	)
+	route.Get("/:id", bookingHandler.HandleGetBooking)
+	req := httptest.NewRequest("GET", fmt.Sprintf("/%s", booking.ID.Hex()), nil)
+	req.Header.Add("X-Api-Token", CreateTokenFromUser(user))
+	resp, err := app.Test(req)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if resp.StatusCode != http.StatusOK {
+		t.Fatalf("non 200 code got %d", resp.StatusCode)
+	}
+	var bookingResp *models.Booking
+	if err := json.NewDecoder(resp.Body).Decode(&bookingResp); err != nil {
+		t.Fatal(err)
+	}
+	if bookingResp.ID != booking.ID {
+		t.Fatalf("expected %s got %s", booking.ID, bookingResp.ID)
+	}
+	if bookingResp.UserID != booking.UserID {
+		t.Fatalf("expected %s got %s", booking.UserID, bookingResp.UserID)
+	}
+
+	req = httptest.NewRequest("GET", fmt.Sprintf("/%s", booking.ID.Hex()), nil)
+	req.Header.Add("X-Api-Token", CreateTokenFromUser(nonAuthUser))
+	resp, err = app.Test(req)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if resp.StatusCode == http.StatusOK {
+		t.Fatalf("expected a non 200 status code got %d", resp.StatusCode)
+	}
+}
+
 func TestGetBookings(t *testing.T) {
 	db := setup(t)
 	defer db.teardown(t)
-	user := fixtures.AddUser(db.Store, "james", "foo", false)
-	hotel := fixtures.AddHotel(db.Store, "bar hotel", "a", 4, nil)
-	room := fixtures.AddRoom(db.Store, "small", true, 4.4, hotel.ID)
-	from := time.Now()
-	till := from.AddDate(0, 0, 5)
-	booking := fixtures.AddBooking(db.Store, user.ID, room.ID, from, till)
-	fmt.Println(booking)
+	var (
+		adminUser      = fixtures.AddUser(db.Store, "admin", "admin", true)
+		user           = fixtures.AddUser(db.Store, "james", "foo", false)
+		hotel          = fixtures.AddHotel(db.Store, "bar hotel", "a", 4, nil)
+		room           = fixtures.AddRoom(db.Store, "small", true, 4.4, hotel.ID)
+		from           = time.Now()
+		till           = from.AddDate(0, 0, 5)
+		booking        = fixtures.AddBooking(db.Store, user.ID, room.ID, from, till)
+		app            = fiber.New()
+		admin          = app.Group("/", middleware.JWTAuthentication(db.User), middleware.AdminAuth)
+		bookingHandler = NewBookingHandler(db.Store)
+	)
+	_ = booking
+	admin.Get("/", bookingHandler.HandleGetBookings)
+	req := httptest.NewRequest("GET", "/", nil)
+	req.Header.Add("X-Api-Token", CreateTokenFromUser(adminUser))
+	resp, err := app.Test(req)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if resp.StatusCode != http.StatusOK {
+		t.Fatalf("non 200 response got %d", resp.StatusCode)
+	}
+	var bookings []*models.Booking
+	if err := json.NewDecoder(resp.Body).Decode(&bookings); err != nil {
+		t.Fatal(err)
+	}
+	if len(bookings) != 1 {
+		t.Fatalf("expected 1 booking got %d", len(bookings))
+	}
+	have := bookings[0]
+	if have.ID != booking.ID {
+		t.Fatalf("expected %s got %s", booking.ID, have.ID)
+	}
+	if have.UserID != booking.UserID {
+		t.Fatalf("expected %s got %s", booking.UserID, have.UserID)
+	}
+
+	// test non-admin cannot access the bookings
+	req = httptest.NewRequest("GET", "/", nil)
+	req.Header.Add("X-Api-Token", CreateTokenFromUser(adminUser))
+	resp, err = app.Test(req)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if resp.StatusCode == http.StatusOK {
+		t.Fatalf("expected a non 200 status code got %d", resp.StatusCode)
+	}
 }
